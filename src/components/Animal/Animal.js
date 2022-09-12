2430b1bf550f1bbafd65e31b83ebf8782a806c3c
Allison Ause <allison.ause@gmail.com>
2022-09-12 14:43:00 -0700

    removed the erroneous type from the text display

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -4,7 +4,7 @@ export default function Animal(props) {
   return (
     <>
       <div className="animal" style={{ top: props.top, left: props.left }}>
-        {props.name} {props.type} {props.says}
+        {props.name} {props.says}
         <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
       </div>
     </>

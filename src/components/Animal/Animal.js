4d50726317b91fb37fcdc9714e34f2c33994d10e
Allison Ause <allison.ause@gmail.com>
2022-09-12 14:37:32 -0700

    styling and layout finished

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,8 +1,11 @@
+import './Animal.css';
+
 export default function Animal(props) {
   return (
     <>
-      <div style={{ top: props.top, left: props.left }}>
-        Animal: {props.name} {props.type} {props.says}
+      <div className="animal" style={{ top: props.top, left: props.left }}>
+        {props.name} {props.type} {props.says}
+        <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
       </div>
     </>
   );
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './Main.css';
 import background from '../../background.png';
-import '../../data.js';
-import Animal from '../Animal/Animal';
+import { animals } from '../../data.js';
+import Animal from '../Animal/Animal.js';
 
 export default function Main() {
-  return <main style={{ backgroundImage: `url(${background})` }}></main>;
+  return (
+    <>
+      <main className="main" style={{ backgroundImage: `url(${background})` }}>
+        {animals.map((animal) => (
+          <Animal key={animal.name} {...animal} />
+          //name={animal.name} type={animal.type} says={animal.says} top={animal.top} left={animal.left}
+        ))}
+      </main>
+    </>
+  );
 }
